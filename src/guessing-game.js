9de04eb07311c3f1a2c0b7db84078e86bcd759cb
Гуськов Владимир <raz3r444uk@mail.ru>
2022-08-07 14:44:54 +0400

    feat: ended project

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,23 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {}
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-    }
+  guess() {
+    this.number = Math.ceil((this.max + this.min) / 2);
+    return this.number;
+  }
 
-    guess() {
+  lower() {
+    this.max = this.number;
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.number;
+  }
 }
 
 module.exports = GuessingGame;
